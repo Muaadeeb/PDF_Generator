d3769eec166a412ebde151d792cef32834e4577f
Timothy Ingledue <tingledue@covisum.com>
2024-07-08 16:18:45 -0500

    Created

---
+++ CreatePdf/Program.cs
@@ -0,0 +1,42 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace CreatePdf;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        QuestPDF.Settings.License = LicenseType.Community;
+
+        Document.Create(container =>
+        {
+            container.Page(page =>
+            {
+                page.Size(PageSizes.A4);
+                page.Margin(2, Unit.Centimetre);
+                page.DefaultTextStyle(x => x.FontSize(20));
+
+                page.Header().Text("Timothy Ingledue - Hello World - Test 3")
+                    .SemiBold().FontSize(30).FontColor(Colors.Amber.Medium);
+
+                page.Content()
+                    .PaddingVertical(1, Unit.Centimetre)
+                    .Column(x =>
+                    {
+                        x.Spacing(20);
+                        x.Item().Text(Placeholders.LoremIpsum());
+                        x.Item().Image(Placeholders.Image(200, 100));
+                    });
+                page.Footer()
+                    .AlignCenter()
+                    .Text(x =>
+                    {
+                        x.Span("Page ");
+                        x.CurrentPageNumber();
+                    });
+            });
+        }).GeneratePdf("TestCreationOfPDF_03.pdf");
+    }
+}
